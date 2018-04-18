6cce8c47f954c9a3b4000057471ccfb09b3532c3
Darrell Pawson <dpawson905@gmail.com>
2018-04-18 19:25:47 -0400

    create user schema and create test file to make sure I can add a user to the DB

---
+++ src/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// This is the schema for the structure of the database
+const UserSchema = new Schema({
+	name: String
+});
+
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
