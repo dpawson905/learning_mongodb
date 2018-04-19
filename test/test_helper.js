8e81451494c4b373eca6347de76ed6ff5c649f32
Darrell Pawson <dpawson905@gmail.com>
2018-04-18 21:24:13 -0400

    set global.Promise to avoid mongoose warnings

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-    .once('open', ()=> console.log('Good to go!'))
-    .on('error', (error) => {
-        console.warn('Warning', error)
-    });
+before((done) => {
+    mongoose.connect("mongodb://localhost/users_test");
+    mongoose.connection
+      .once("open", () => { done(); })
+      .on("error", error => {
+        console.warn("Warning", error);
+      });
+});
 
 beforeEach((done) => {
     // this will delete all users before running the test
