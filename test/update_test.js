84596855130356c15304be0e10e65b2c327d5431
Darrell Pawson <dpawson905@gmail.com>
2018-04-20 21:04:01 -0400

    finish validation testing and set up virtual types

--- test/update_test.js
+++ test/update_test.js
@@ -7,7 +7,7 @@ describe("Updating records", () => {
   beforeEach(done => {
     joe = new User({
       name: "Joe",
-      postCount: 0
+      likes: 0
     });
     joe.save().then(() => done());
   });
@@ -45,10 +45,10 @@ describe("Updating records", () => {
   });
 
   it("A user can have their post count incremented by 1", (done) => {
-    User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+    User.update({ name: 'Joe' }, { $inc: { likes: 1 } })
       .then(() => User.findOne({ name: 'Joe'}))
       .then((user) => {
-        assert(user.postCount === 1);
+        assert(user.likes === 1);
         done();
       });
   });
